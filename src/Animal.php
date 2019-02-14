5eba5beffa8c7293b76bfd6bc3678c1af2a0df82
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-02-14 11:50:37 +0100

    création de la class astraite Animal

---
+++ src/Animal.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+abstract class Animal{
+
+    private $name;
+
+    public function __construct($name){
+        $this->name=$name;
+    }
+
+    public function __getName(){
+        return $this->name;
+    }
+
+    protected abstract function getNoise(): string;
+
+    public function noise(): string{
+       return$this->getNoise;
+    }
+}
